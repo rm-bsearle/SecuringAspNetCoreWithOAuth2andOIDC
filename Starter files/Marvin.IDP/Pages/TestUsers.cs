3acca794eea9e595ef11bc37625d784fc173affd
rm-bsearle <briansearle@rocket.com>
2026-08-24 15:27:26 -0700

    Adding Users to Test with

--- Starter files/Marvin.IDP/Pages/TestUsers.cs
+++ Starter files/Marvin.IDP/Pages/TestUsers.cs
@@ -56,6 +56,28 @@ public static class TestUsers
                         new Claim(JwtClaimTypes.WebSite, "http://bob.com"),
                         new Claim(JwtClaimTypes.Address, JsonSerializer.Serialize(address), IdentityServerConstants.ClaimValueTypes.Json)
                     }
+                },
+                new TestUser
+                {
+                    SubjectId = "d860efca-22d9-47fd-8249-791ba61b07c7",
+                    Username = "David",
+                    Password = "password",
+                    Claims =
+                    {
+                            new Claim(JwtClaimTypes.GivenName, "David"),
+                            new Claim(JwtClaimTypes.FamilyName, "Flagg"),
+                    }
+                },
+                new TestUser
+                {
+                    SubjectId = "b7539694-97e7-4dfe-84da-b4256e1ff5c7",
+                    Username = "Emma",
+                    Password = "password",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.GivenName, "Emma"),
+                        new Claim(JwtClaimTypes.FamilyName, "Flagg"),
+                    }
                 }
             };
         }
