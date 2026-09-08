e966b6c63d7a3f862688e2c89cb927a73a1436fb
rm-bsearle <briansearle@rocket.com>
2026-09-08 11:23:18 -0700

    Securing Access to Your API pt.1

--- Starter files/Marvin.IDP/Config.cs
+++ Starter files/Marvin.IDP/Config.cs
@@ -13,6 +13,12 @@ public static class Config
             new IdentityResource("roles", "Your role(s)", ["role"])
         };
 
+    public static IEnumerable<ApiResource> ApiResources =>
+        new ApiResource[]
+        {
+            new ApiResource("imagegalleryapi", "Image Gallery API")
+        };
+
     public static IEnumerable<ApiScope> ApiScopes =>
         new ApiScope[]
             { };
@@ -38,6 +44,7 @@ public static class Config
                     IdentityServerConstants.StandardScopes.OpenId,
                     IdentityServerConstants.StandardScopes.Profile,
                     "roles",
+                    "imagegalleryapi",
                 },
                 ClientSecrets =
                 {
