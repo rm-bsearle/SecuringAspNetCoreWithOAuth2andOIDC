58c255b80160374a8f4309b9b547582258628e6a
rm-bsearle <briansearle@rocket.com>
2026-09-23 13:28:14 -0700

    Using Custom Attributes for Authorization

---
+++ Starter files/ImageGallery.API/Authorization/MustOwnImageAttribute.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Authorization;
+
+namespace ImageGallery.API.Authorization;
+
+public class MustOwnImageAttribute : AuthorizeAttribute, IAuthorizationRequirementData
+{
+    public IEnumerable<IAuthorizationRequirement> GetRequirements()
+    {
+        return [new MustOwnImageRequirement()];
+    }
+}
